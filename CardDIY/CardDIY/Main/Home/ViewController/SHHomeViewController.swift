6f8c597b7d48bb777e7428c558b5c911a0e9536d
shvier <shvier@shviers-iMac.local>
2017-07-04 15:05:26 +0800

    Init Project

---
+++ CardDIY/CardDIY/Main/Home/ViewController/SHHomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SHHomeViewController.swift
+//  CardDIY
+//
+//  Created by shvier on 04/07/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import UIKit
+
+class SHHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
