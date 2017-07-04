5fcf2d7bd116af4e41e1b140ee69e32aa345cb63
shvier <shvier@shviers-iMac.local>
2017-07-04 15:37:19 +0800

    Add CommonMacro

---
+++ CardDIY/CardDIY/Global/Const/CommonMacro.swift
@@ -0,0 +1,11 @@
+//
+//  CommonMacro.swift
+//  CardDIY
+//
+//  Created by shvier on 04/07/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import UIKit
+
+let kNavigationAndStatusBarHeight: CGFloat = 64
--- CardDIY/CardDIY/Main/Home/ViewController/SHHomeViewController.swift
+++ CardDIY/CardDIY/Main/Home/ViewController/SHHomeViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import SnapKit
 
 class SHHomeViewController: UIViewController {
+    
+    let backgroundAlpha: CGFloat = 0.5
+    
+    var backgroundImageView: UIImageView?
     
     func leftBarItemAction(sender: UIBarButtonItem) {
         sideMenuViewController.presentLeftMenuViewController()
@@ -16,11 +21,26 @@ class SHHomeViewController: UIViewController {
     
     func initUI() {
         configNavi()
+        
+        backgroundImageView = ({
+            let imageView = UIImageView()
+            imageView.image = UIImage(named: "ygo_background.jpg")
+            imageView.contentMode = .scaleAspectFill
+            imageView.alpha = backgroundAlpha
+            return imageView
+        })()
+        view.addSubview(backgroundImageView!)
+        
+        backgroundImageView?.snp.makeConstraints({ (make) in
+            make.left.right.bottom.equalTo(view)
+            make.top.equalTo(view).offset(kNavigationAndStatusBarHeight)
+        })
     }
     
     func configNavi() {
         let leftBarItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(self.leftBarItemAction(sender:)))
         navigationItem.leftBarButtonItem = leftBarItem
+        navigationItem.title = "游戏王"
     }
 
     override func viewDidLoad() {
