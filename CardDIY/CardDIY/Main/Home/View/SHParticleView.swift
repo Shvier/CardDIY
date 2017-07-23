435643e60c6b67584836e7bf722d353b7ab4f187
Shvier <daulvi@hotmail.com>
2017-07-23 21:19:58 +0800

    Add ParticleView

---
+++ CardDIY/CardDIY/Main/Home/View/SHParticleView.swift
@@ -0,0 +1,18 @@
+//
+//  SHParticleView.swift
+//  CardDIY
+//
+//  Created by Shvier on 23/07/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import UIKit
+
+class SHParticleView: SHBaseView {
+
+    var starCell: CAEmitterCell!
+    var circleCell: CAEmitterCell!
+    
+    var particleLayer: CAEmitterLayer!
+    
+}
