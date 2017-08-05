4dd35b7cfa8d7862a34fdb65b5e188be2ac15101
Shvier <daulvi@hotmail.com>
2017-08-05 20:55:38 +0800

    Add Card Color

--- CardDIY/CardDIY/Global/Const/CommonMacro.swift
+++ CardDIY/CardDIY/Global/Const/CommonMacro.swift
@@ -21,6 +21,15 @@ let UMAppKey = "59673662717c1903a50001eb"
 let AdMobID = "ca-app-pub-7779776531531575~7426958844"
 let AppUrl = ""
 
+let NormalMonsterColor  = UIColor(red:0.68, green:0.55, blue:0.38, alpha:1.00)
+let EffectMonsterColor  = UIColor(red:0.68, green:0.37, blue:0.25, alpha:1.00)
+let RitualMonsterColor  = UIColor(red:0.33, green:0.48, blue:0.71, alpha:1.00)
+let FusionMonsterColor  = UIColor(red:0.47, green:0.31, blue:0.80, alpha:1.00)
+let SynchroMonsterColor = UIColor(red:0.87, green:0.87, blue:0.87, alpha:1.00)
+let XYZMonsterColo      = UIColor(red:0.33, green:0.33, blue:0.33, alpha:1.00)
+let MagicColor          = UIColor(red:0.20, green:0.62, blue:0.50, alpha:1.00)
+let TrapColor           = UIColor(red:0.70, green:0.28, blue:0.50, alpha:1.00)
+
 func IsiPhone4() -> Bool {
     return ScreenHeight == iPhone4Height
 }
